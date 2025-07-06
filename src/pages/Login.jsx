9fc6a3c2719e21ff383c910c360bdb7e12d36d2a
Tai Dev <tantailg2003@gmail.com>
2025-07-06 20:45:14 +0700

    add login with google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebaseConfig";
 
 export default function Login({ onLoginSuccess, apiUrl, user, setUser, isLoggedIn, setIsLoggedIn}) {
     const [username, setUsername] = useState("");
@@ -54,6 +55,36 @@ export default function Login({ onLoginSuccess, apiUrl, user, setUser, isLoggedI
 
   };
 
+    const handleGoogleLogin = async () => {
+    setError("");
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const idToken = await result.user.getIdToken();
+
+      // Gửi idToken về Flask backend
+      const res = await fetch(`${apiUrl}/auth/google`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ idToken })
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        localStorage.setItem('token', data.access_token); // giả sử backend trả JWT
+        localStorage.setItem('username', data.username || result.user.email);
+        handleLoginSuccess();
+      } else {
+        setError(data.msg || data.message || "Đăng nhập Google thất bại");
+      }
+
+    } catch (err) {
+      console.error(err);
+      setError("Lỗi đăng nhập Google");
+    }
+  };
+
+
   return (
     <div className="card p-4 shadow bg-white" style={{ maxWidth: "400px", margin: "0 auto" }}>
       <h2 className="text-center mb-4">Đăng nhập</h2>
@@ -95,6 +126,11 @@ export default function Login({ onLoginSuccess, apiUrl, user, setUser, isLoggedI
         </form> &nbsp;
       <button className="btn btn-success" onClick={() => navigate("/signup")}>
           Bạn chưa có tài khoản? Đăng ký ngay tại đây.
+      </button>
+              <br />
+      <button class="google-btn" onclick="handleGoogleLogin()">
+        <img src="https://developers.google.com/identity/images/g-logo.png" class="google-icon" alt="Google logo" />
+        <span>Đăng nhập bằng Google</span>
       </button>
         <br />
       <button className="btn btn-sm btn-outline-secondary mt-2" onClick={() => navigate("/")}>
